eb2332b921c22a1569c7279a5ce7159d4decf0b1
rpo <pozzir00@gmail.com>
2021-08-13 00:51:28 +0200

    Eval starting from all features (LR)

--- notebooks/nil_eval.py
+++ notebooks/nil_eval.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import sys
 import itertools
+import re
 
 import click
 
@@ -19,15 +20,15 @@ def _eval(df, y_pred, y='y', title=None, threshold=0.5):
     F1 = 2*(PPV*TPR)/(PPV+TPR)
     return pd.DataFrame({
         "ACC": [ACC],
+        "F1": [F1],  
         "TPR": [TPR],
         "TNR": [TNR],
+        "FPR": [FPR],
         "FNR": [FNR],
         "PPV": [PPV],
         "NPV": [NPV],
-        "FPR": [FPR],
         "FDR": [FDR],
         "FOR": [FOR],
-        "F1": [F1],        
     }, index = [y_pred if title is None else title])
 
 def _cm(df, y_pred, y='y', threshold=0.5):
@@ -74,6 +75,23 @@ def _lr_double(datasets, x1, x2):
 
             yield _eval(_temp, 'y_pred', title=f'{x1}+{x2}+{k}+{tk}')
 
+def _lr_all(datasets, _filter=None, title='all'):
+    for k in ['train', 'train_hard', 'train_aug', 'train_hard_aug']:
+        d = datasets[k]
+        cols = [c for c in d.columns
+                    if c not in ['y']
+                        and not c.startswith('correct')
+                        and (_filter is None or re.match(_filter, c))]
+        mdl = LinearRegression().fit(d[cols].values.reshape(-1, len(cols)), d['y'])
+
+        for tk in ['test', 'test_hard']:
+            t = datasets[tk]
+            _temp = pd.DataFrame()
+            _temp['y'] = t['y']
+            _temp['y_pred'] = mdl.predict(t[cols].values.reshape(-1, len(cols)))
+
+            yield _eval(_temp, 'y_pred', title=f'{title}+{k}+{tk}')
+
 @click.command()
 @click.option('--data-path', required=True, type=str, help='path in which to find datasets')
 @click.option('--save-results', required=False, type=str, default=None, help='whether and where to save results')
@@ -89,6 +107,9 @@ def main(data_path, save_results):
         _lr_single(datasets, 'max_bi'),
         _lr_single(datasets, 'max_cross'),
         _lr_double(datasets, 'max_bi', 'max_cross'),
+        _lr_all(datasets, '.*_bi', 'all_bi'),
+        _lr_all(datasets, '.*_cross', 'all_cross'),
+        _lr_all(datasets),
         ):
         results = pd.concat([results, _df])
 
