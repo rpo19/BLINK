45ede161e3dbb15ec0e2a837dc7392e0d2f7b30d
rpo <pozzir00@gmail.com>
2021-08-13 12:37:32 +0200

    Add metrics, Fix augmentation

--- notebooks/nil_eval.py
+++ notebooks/nil_eval.py
@@ -4,31 +4,55 @@ from sklearn.linear_model import LinearRegression
 import sys
 import itertools
 import re
+import math
 
 import click
 
+def _fbeta(b, PPV, TPR):
+    f = (1+b**2) * PPV * TPR / (b**2*PPV + TPR + sys.float_info.min)
+    return f
+
 def _eval(df, y_pred, y='y', title=None, threshold=0.5):
-    ACC = df.query(f'(y==1 and {y_pred}>={threshold}) or (y==0 and {y_pred}<{threshold})').count()[0] / (sys.float_info.min + df.shape[0])
-    TPR = df.query(f'y==1 and {y_pred} >= {threshold}').count()[0] / (sys.float_info.min + df.query(f'y>={threshold}').shape[0])
-    TNR = df.query(f'y==0 and {y_pred} < {threshold}').count()[0] / (sys.float_info.min + df.query(f'y<{threshold}').shape[0])
+    tot = df.shape[0]
+    TP = df.query(f'y==1 and {y_pred} >= {threshold}').count()[0]
+    TN = df.query(f'y==0 and {y_pred} < {threshold}').count()[0]
+    FP = df.query(f'y==0 and {y_pred} >= {threshold}').count()[0]
+    FN = df.query(f'y==1 and {y_pred} < {threshold}').count()[0]
+    assert tot == TP + TN + FP + FN
+    ACC = (TP + TN) / (sys.float_info.min + tot)
+    TPR = TP / (sys.float_info.min + TP + FN)
+    TNR = TN / (sys.float_info.min + TN + FP)
     FNR = 1 - TPR
-    PPV = df.query(f'y==1 and {y_pred} >= {threshold}').count()[0] / (sys.float_info.min + df.query(f'{y_pred}>={threshold}').shape[0])
-    NPV = df.query(f'y==0 and {y_pred} < {threshold}').count()[0] / (sys.float_info.min + df.query(f'{y_pred}<{threshold}').shape[0])
+    PPV = TP / (sys.float_info.min + TP + FP)
+    NPV = TN / (sys.float_info.min + TN + FN)
     FPR = 1 - TNR
-    FDR = 1 - PPV
-    FOR = 1 - NPV
-    F1 = 2*(PPV*TPR)/(PPV+TPR)
+    # FDR = 1 - PPV
+    # FOR = 1 - NPV
+    F1 = _fbeta(1, PPV, TPR)
+    F2 = _fbeta(2, PPV, TPR)
+    F05 = _fbeta(0.5, PPV, TPR)
+    FN1 = _fbeta(1, NPV, TNR)
+    FN2 = _fbeta(2, NPV, TNR)
+    FN05 = _fbeta(1, NPV, TNR)
+    MCC = (TP*TN - FP*FN) / (sys.float_info.min + math.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)))
+
     return pd.DataFrame({
         "ACC": [ACC],
-        "F1": [F1],  
+        "MCC": [MCC],
+        "F1": [F1],
+        "F2": [F2],
+        "F05": [F05],
+        "FN1": [FN1],
+        "FN2": [FN2],
+        "FN05": [FN05],
         "TPR": [TPR],
         "TNR": [TNR],
         "FPR": [FPR],
         "FNR": [FNR],
         "PPV": [PPV],
         "NPV": [NPV],
-        "FDR": [FDR],
-        "FOR": [FOR],
+        # "FDR": [FDR],
+        # "FOR": [FOR],
     }, index = [y_pred if title is None else title])
 
 def _cm(df, y_pred, y='y', threshold=0.5):
@@ -60,7 +84,7 @@ def _lr_single(datasets, x):
             _temp['y'] = t['y']
             _temp['y_pred'] = mdl.predict(t[x].values.reshape(-1, 1))
 
-            yield _eval(_temp, 'y_pred', title=f'{x}+{k}+{tk}')
+            yield _eval(_temp, 'y_pred', title=f'lr_{x}+{k}+{tk}')
 
 def _lr_double(datasets, x1, x2):
     for k in ['train', 'train_hard', 'train_aug', 'train_hard_aug']:
@@ -73,36 +97,62 @@ def _lr_double(datasets, x1, x2):
             _temp['y'] = t['y']
             _temp['y_pred'] = mdl.predict(t[[x1, x2]].values.reshape(-1, 2))
 
-            yield _eval(_temp, 'y_pred', title=f'{x1}+{x2}+{k}+{tk}')
+            yield _eval(_temp, 'y_pred', title=f'lr_{x1}+{x2}+{k}+{tk}')
 
 def _lr_all(datasets, _filter=None, title='all'):
     for k in ['train', 'train_hard', 'train_aug', 'train_hard_aug']:
         d = datasets[k]
         cols = [c for c in d.columns
-                    if c not in ['y']
+                    if c not in ['y', 'idx']
                         and not c.startswith('correct')
                         and (_filter is None or re.match(_filter, c))]
+        #print('_lr_all selected columns:', cols)
         mdl = LinearRegression().fit(d[cols].values.reshape(-1, len(cols)), d['y'])
 
-        for tk in ['test', 'test_hard']:
+        for tk in ['test', 'test_hard', 'test_aug', 'test_hard_aug']:
             t = datasets[tk]
             _temp = pd.DataFrame()
             _temp['y'] = t['y']
             _temp['y_pred'] = mdl.predict(t[cols].values.reshape(-1, len(cols)))
 
-            yield _eval(_temp, 'y_pred', title=f'{title}+{k}+{tk}')
+            yield _eval(_temp, 'y_pred', title=f'lr_{title}+{k}+{tk}')
+
+def _print_results(results_df, sort=None, grep=None, reverse=False):
+    to_print = results_df.copy()
+    if grep:
+        to_print = to_print.loc[[i for i in to_print.index if re.match(grep, i)]]
+    if sort:
+        to_print = to_print.sort_values(by=sort, ascending=reverse)
+    to_print = to_print.round(decimals=3)
+    print(to_print.to_markdown())
 
 @click.command()
-@click.option('--data-path', required=True, type=str, help='path in which to find datasets')
-@click.option('--save-results', required=False, type=str, default=None, help='whether and where to save results')
-def main(data_path, save_results):
+@click.option('-d', '--data-path', required=False, type=str, help='path in which to find datasets')
+@click.option('-r', '--results', required=False, type=str, default=None, help='whether and where to save results')
+@click.option('-s', '--sort', required=False, type=str, default=None, help='sort by metric')
+@click.option('--reverse', required=False, default=False, is_flag=True, help='reverse sort order')
+@click.option('-f', '--fast', required=False, default=False, is_flag=True, help='do not calculate but load from --results and show')
+@click.option('-g', '--grep', required=False, default=None, type=str, help='regex to filter index')
+def main(data_path, results, sort, fast, grep, reverse):
+
+    if fast and results is None:
+        raise Exception('--fast requires --results to load dataset')
+
+    if fast:
+        results_df = pd.read_csv(results, index_col=0)
+        _print_results(results_df, sort, grep, reverse)
+        sys.exit(0)
+
+    if not data_path:
+        raise Exception('--data-path not set!')
+
     datasets = _load_datasets(data_path)
-    
+
     # train + train_hard + train_aug + train_hard_aug (4)
 
     # test + test_hard (2)
 
-    results = pd.DataFrame()
+    results_df = pd.DataFrame()
     for _df in itertools.chain(
         _lr_single(datasets, 'max_bi'),
         _lr_single(datasets, 'max_cross'),
@@ -111,12 +161,12 @@ def main(data_path, save_results):
         _lr_all(datasets, '.*_cross', 'all_cross'),
         _lr_all(datasets),
         ):
-        results = pd.concat([results, _df])
+        results_df = pd.concat([results_df, _df])
 
-    print(results.to_markdown())
+    _print_results(results_df, sort, grep, reverse)
 
-    if save_results is not None:
-        results.to_csv(save_results, index=True) 
+    if results is not None:
+        results_df.to_csv(results, index=True)
 
 if __name__ == '__main__':
     main()
--- notebooks/prepare_dataset.py
+++ notebooks/prepare_dataset.py
@@ -8,43 +8,50 @@ import statistics
 
 import click
 
-def _bi_get_stats(x):
+# TODO consider removing top_n to create easier augmented instances
+def _bi_get_stats(x, remove_correct = False):
     assert len(x.scores) == len(x.nns)
+    scores = x.scores.copy()
+    correct = None
     if x.labels in x.nns:
         # found correct entity
         i_correct = x.nns.index(x.labels)
-        correct = x.scores[i_correct]
-    else:
-        # not found correct entity
-        correct = None
+        if remove_correct:
+            del scores[i_correct]
+        else:
+            correct = scores[i_correct]
+
     _stats = {
         "correct": correct,
-        "max": max(x.scores),
-        "second": sorted(x.scores, reverse=True)[1],
-        "min": min(x.scores),
-        "mean": statistics.mean(x.scores),
-        "median": statistics.median(x.scores),
-        "stdev": statistics.stdev(x.scores),
+        "max": max(scores),
+        "second": sorted(scores, reverse=True)[1],
+        "min": min(scores),
+        "mean": statistics.mean(scores),
+        "median": statistics.median(scores),
+        "stdev": statistics.stdev(scores)
     }
     return _stats
 
-def _cross_get_stats(x):
+def _cross_get_stats(x, remove_correct=False):
     assert len(x.unsorted_scores) == len(x.nns)
+    scores = x.unsorted_scores.copy()
+    correct = None
     if x.labels in x.nns:
         # found correct entity
         i_correct = x.nns.index(x.labels)
-        correct = x.unsorted_scores[i_correct]
-    else:
-        # not found correct entity
-        correct = None
+        if remove_correct:
+            del scores[i_correct]
+        else:
+            correct = scores[i_correct]
+
     _stats = {
         "correct": correct,
-        "max": max(x.unsorted_scores),
-        "second": sorted(x.unsorted_scores, reverse=True)[1],
-        "min": min(x.unsorted_scores),
-        "mean": statistics.mean(x.unsorted_scores),
-        "median": statistics.median(x.unsorted_scores),
-        "stdev": statistics.stdev(x.unsorted_scores),
+        "max": max(scores),
+        "second": sorted(scores, reverse=True)[1],
+        "min": min(scores),
+        "mean": statistics.mean(scores),
+        "median": statistics.median(scores),
+        "stdev": statistics.stdev(scores),
     }
     return _stats
 
@@ -78,7 +85,7 @@ def _load_scores(score_path):
 
     combined_stats['idx'] = combined_stats.index
 
-    return bi_stats, cross_stats, combined_stats
+    return bi_stats, cross_stats, combined_stats, bi_df, cross_df
 
 def _load_nil(nil_path):
     bi_scores_nil = sorted(glob(os.path.join(nil_path, '*_bi.jsonl')))
@@ -87,18 +94,18 @@ def _load_nil(nil_path):
     bi_df_nil = pd.DataFrame()
     for score_file in bi_scores_nil:
         bi_df_nil = pd.concat([bi_df_nil, pd.read_json(score_file)])
-        
+
     assert (bi_df_nil['labels'].apply(lambda x: len(x)) != 1).sum() == 0
     bi_df_nil['labels'] = bi_df_nil['labels'].apply(lambda x: x[0])
 
     bi_stats_nil = bi_df_nil.apply(_bi_get_stats, axis=1, result_type='expand')
-    
+
     assert bi_stats_nil['correct'].notna().sum() == 0
 
     cross_df_nil = pd.DataFrame()
     for score_file in cross_scores_nil:
         cross_df_nil = pd.concat([cross_df_nil, pd.read_json(score_file)])
-        
+
     assert (cross_df_nil['labels'].apply(lambda x: len(x)) != 1).sum() == 0
     cross_df_nil['labels'] = cross_df_nil['labels'].apply(lambda x: x[0])
 
@@ -110,7 +117,7 @@ def _load_nil(nil_path):
 
     combined_stats_nil['idx'] = combined_stats_nil.index
 
-    return bi_stats_nil, cross_stats_nil, combined_stats_nil
+    return bi_stats_nil, cross_stats_nil, combined_stats_nil, bi_df_nil, cross_df_nil
 
 def _bi_cross_errors(combined_stats):
     print('bi errors', combined_stats.query('correct_bi < max_bi').count()[0])
@@ -164,17 +171,33 @@ def _train_test(dataset, create_validation=False):
         train, validation = train_test_split(train, test_size=0.2, random_state=42)
     return train, test, validation
 
-def _augment(dataset, combined_stats):
+def _augment(dataset, combined_stats, bi_df, cross_df):
     n_augment = (lambda x: abs(x[0]-x[1]))(dataset['y'].value_counts())
 
     _temp_stats = combined_stats.iloc[dataset['idx']]
     pos_to_sample = _temp_stats[_temp_stats['correct_bi'].notna()].query('correct_bi >= max_bi and correct_cross >= max_cross')
 
-    aug_negatives = pos_to_sample.iloc[sample_without_replacement(pos_to_sample.shape[0], n_augment, random_state=42)].copy()
-    aug_negatives['y'] = [0] * aug_negatives.shape[0]
-    print('augment shape', aug_negatives.shape)
+    aug_negatives_idx = sample_without_replacement(pos_to_sample.shape[0], n_augment, random_state=42)
 
-    dataset_aug = pd.concat([dataset, aug_negatives])
+    bi2aug = bi_df.iloc[aug_negatives_idx]
+    cross2aug = cross_df.iloc[aug_negatives_idx]
+
+    bi_aug_stats = bi2aug.apply(lambda x: _bi_get_stats(x, remove_correct=True), axis=1, result_type='expand')
+    cross_aug_stats = cross2aug.apply(lambda x: _cross_get_stats(x, remove_correct=True), axis=1, result_type='expand')
+
+    combined_aug_stats = bi_aug_stats.copy()
+    combined_aug_stats.columns = [c+'_bi' for c in combined_aug_stats.columns]
+    combined_aug_stats[[c+'_cross' for c in cross_aug_stats.columns]] = cross_aug_stats
+
+    combined_aug_stats['idx'] = combined_aug_stats.index
+
+    combined_aug_stats['y'] = [0] * combined_aug_stats.shape[0]
+    print('augment shape', combined_aug_stats.shape)
+
+    assert combined_aug_stats['correct_bi'].notna().sum() == 0
+    assert combined_aug_stats['correct_cross'].notna().sum() == 0
+
+    dataset_aug = pd.concat([dataset, combined_aug_stats])
     print('augmented shape', dataset_aug.shape)
     print('augmented distr:\n', dataset_aug['y'].value_counts())
 
@@ -182,7 +205,7 @@ def _augment(dataset, combined_stats):
 
 def _load_datasets(path):
     datasets = {}
-    for d in ['train', 'test', 'train_hard', 'test_hard', 'train_aug', 'train_hard_aug']:
+    for d in ['train', 'test', 'train_hard', 'test_hard', 'train_aug', 'train_hard_aug', 'test_aug', 'test_hard_aug']:
         datasets[d] = pd.read_csv(os.path.join(path, d))
     return datasets
 
@@ -192,17 +215,20 @@ def _load_datasets(path):
 @click.option('--nil-path', required=True, type=str, help='path in which to find nil score files')
 @click.option('--out-path', required=True, type=str, help='output path in which to save dataset')
 def main(score_path, nil_path, out_path):
-    _,_,combined_stats = _load_scores(score_path)
+    _,_,combined_stats, bi_df, cross_df = _load_scores(score_path)
     print('score shape', combined_stats.shape)
-    _,_,combined_stats_nil = _load_nil(nil_path)
+    _,_,combined_stats_nil,_,_ = _load_nil(nil_path)
     print('nil shape', combined_stats_nil.shape)
 
     _bi_cross_errors(combined_stats)
 
     train, test, train_hard, test_hard = _create_dataset(combined_stats, combined_stats_nil)
 
-    train_aug = _augment(train, combined_stats)
-    train_hard_aug = _augment(train_hard, combined_stats)
+    train_aug = _augment(train, combined_stats, bi_df, cross_df)
+    train_hard_aug = _augment(train_hard, combined_stats, bi_df, cross_df)
+
+    test_aug = _augment(test, combined_stats, bi_df, cross_df)
+    test_hard_aug = _augment(test_hard, combined_stats, bi_df, cross_df)
 
     assert train['max_bi'].isna().sum() == 0
     assert train['max_cross'].isna().sum() == 0
@@ -220,6 +246,12 @@ def main(score_path, nil_path, out_path):
     assert train_aug['max_cross'].isna().sum() == 0
     assert train_hard_aug['max_bi'].isna().sum() == 0
     assert train_hard_aug['max_cross'].isna().sum() == 0
+    assert test_aug['max_bi'].isna().sum() == 0
+    assert test_aug['max_cross'].isna().sum() == 0
+    assert test_hard_aug['max_bi'].isna().sum() == 0
+    assert test_hard_aug['max_cross'].isna().sum() == 0
+    assert test_aug['y'].isna().sum() == 0
+    assert test_hard_aug['y'].isna().sum() == 0
 
     # shuffle
     train = train.sample(frac=1, random_state=42)
@@ -228,6 +260,9 @@ def main(score_path, nil_path, out_path):
     test_hard = test_hard.sample(frac=1, random_state=42)
     train_aug = train_aug.sample(frac=1, random_state=42)
     train_hard_aug = train_hard_aug.sample(frac=1, random_state=42)
+    test_aug = test_aug.sample(frac=1, random_state=42)
+    test_hard_aug = test_hard_aug.sample(frac=1, random_state=42)
+
 
     if not os.path.isdir(out_path):
         os.mkdir(out_path)
@@ -238,6 +273,8 @@ def main(score_path, nil_path, out_path):
     test_hard.to_csv(os.path.join(out_path, 'test_hard'), index = False)
     train_aug.to_csv(os.path.join(out_path, 'train_aug'), index = False)
     train_hard_aug.to_csv(os.path.join(out_path, 'train_hard_aug'), index = False)
+    test_aug.to_csv(os.path.join(out_path, 'test_aug'), index = False)
+    test_hard_aug.to_csv(os.path.join(out_path, 'test_hard_aug'), index = False)
 
 if __name__ == '__main__':
     main()
