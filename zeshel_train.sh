8d72f8ac95885a8d65050edac54b8591a5b0c3e1
Riccardo Pozzi <pozzir00@gmail.com>
2021-08-24 11:52:13 +0000

    Set batch size to 16 for zeshel successfully

--- zeshel_train.sh
+++ zeshel_train.sh
@@ -2,5 +2,5 @@
 # based on paper appendix A.2 and params read from blink/biencoder/train_biencoder.py
 #PYTHONPATH=. python blink/biencoder/train_biencoder.py --evaluate --output_eval_file examples/zeshel/data/zeshel/output_eval.txt --train_batch_size 128 --learning_rate 2e-5 --num_train_epochs 5 --max_context_length 128 --output_path examples/zeshel/data/zeshel/output/output_eval.txt --seed 42 --data_path examples/zeshel/data/zeshel/blink_format # cuda out of mem
 # trying with batch_size 64
-PYTHONPATH=. python blink/biencoder/train_biencoder.py --evaluate --output_eval_file examples/zeshel/data/zeshel/output_eval.txt --train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 5 --max_context_length 128 --output_path examples/zeshel/data/zeshel/output/output_eval.txt --seed 42 --data_path examples/zeshel/data/zeshel/blink_format
+PYTHONPATH=. python blink/biencoder/train_biencoder.py --evaluate --output_eval_file examples/zeshel/data/zeshel/output_eval.txt --train_batch_size 16 --learning_rate 2e-5 --num_train_epochs 5 --max_context_length 128 --output_path examples/zeshel/data/zeshel/output/output_eval.txt --seed 42 --data_path examples/zeshel/data/zeshel/blink_format
 # failed even with 32
