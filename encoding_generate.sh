ca5dc6b6d94c8f230c40d22db01368ebabd4fe31
Riccardo Pozzi <pozzir00@gmail.com>
2021-10-01 22:35:19 +0000

    Use flat index when gen encodings.
    There`s a faster way to gen encoding in the notebook.

--- encoding_generate.sh
+++ encoding_generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python blink/main_dense.py --faiss_index hnsw --index_path models/faiss_hnsw_index.pkl --test_mentions data/BLINK_benchmark/AIDA-YAGO2_testa.jsonl --fast --save_encodings output/encodings/AIDA-YAGO2_testa_encodings.jsonl --keep_all --consider_all
-python blink/main_dense.py --faiss_index hnsw --index_path models/faiss_hnsw_index.pkl --test_mentions data/BLINK_benchmark/AIDA-YAGO2_testb.jsonl --fast --save_encodings output/encodings/AIDA-YAGO2_testb_encodings.jsonl --keep_all --consider_all
-python blink/main_dense.py --faiss_index hnsw --index_path models/faiss_hnsw_index.pkl --test_mentions data/BLINK_benchmark/AIDA-YAGO2_train.jsonl --fast --save_encodings output/encodings/AIDA-YAGO2_train_encodings.jsonl --keep_all --consider_all
+python blink/main_dense.py --faiss_index flat --index_path models/faiss_flat_index.pkl --test_mentions data/BLINK_benchmark/AIDA-YAGO2_testa.jsonl --fast --save_encodings output/encodings/AIDA-YAGO2_testa_encodings.jsonl --keep_all --consider_all
+python blink/main_dense.py --faiss_index flat --index_path models/faiss_flat_index.pkl --test_mentions data/BLINK_benchmark/AIDA-YAGO2_testb.jsonl --fast --save_encodings output/encodings/AIDA-YAGO2_testb_encodings.jsonl --keep_all --consider_all
+python blink/main_dense.py --faiss_index flat --index_path models/faiss_flat_index.pkl --test_mentions data/BLINK_benchmark/AIDA-YAGO2_train.jsonl --fast --save_encodings output/encodings/AIDA-YAGO2_train_encodings.jsonl --keep_all --consider_all
