a2796aceccc51c2b170d19d5975a1d2ac1e3b86e
Riccardo Pozzi <pozzir00@gmail.com>
2021-12-09 14:57:00 +0000

    Improve indexer micros

--- microservices/indexer/__main__.py
+++ microservices/indexer/__main__.py
@@ -29,23 +29,23 @@ async def search(input_: Input):
     encodings = input_.encodings
     top_k = input_.top_k
     encodings = [vector_decode(e) for e in encodings]
-    candidates = []
+    all_candidates = []
     for index in indexes:
         indexer = index['indexer']
         scores, candidates = indexer.search_knn(encodings, top_k)
-        for _score, _cand in zip(scores, candidates):
-            candidates.append({
-                'raw_score': _score,
-                'candidate': _cand,
-                'indexer': indexer['name'],
-                'score': _score # is hnsw compute dot-prod # entity encs are required
+        for _scores, _cands in zip(scores, candidates):
+            all_candidates.append({
+                'raw_scores': _scores.tolist(),
+                'candidates': _cands.tolist(),
+                'indexer': index['name'],
+                'scores': _scores.tolist() # is hnsw compute dot-prod # entity encs are required
             })
-    return candidates
+    return all_candidates
 
 
 def load_models(args):
-    assert args.indexes is not None, 'Error! Index is required.'
-    for index in args.indexes.split(','):
+    assert args.index is not None, 'Error! Index is required.'
+    for index in args.index.split(','):
         index_type, index_path, name, rorw = index.split(':')
         print('Loading {} index from {}, mode: {}...'.format(index_type, index_path, rorw))
         if index_type == "flat":
@@ -59,7 +59,7 @@ def load_models(args):
             'indexer': indexer,
             'name': name,
             'path': index_path
-            )
+            })
 
         if rorw == 'rw':
             assert rw_index is None, 'Error! Only one rw index is accepted.'
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # indexer
     parser.add_argument(
-        "--indexes", type=str, default=None, help="comma separate list of paths to load indexes [type:path:name:ro/rw] (e.g: hnsw:index.pkl:wiki:ro,flat:index2.pkl:local:rw)",
+        "--index", type=str, default=None, help="comma separate list of paths to load indexes [type:path:name:ro/rw] (e.g: hnsw:index.pkl:wiki:ro,flat:index2.pkl:local:rw)",
     )
     parser.add_argument(
         "--host", type=str, default="127.0.0.1", help="host to listen at",
