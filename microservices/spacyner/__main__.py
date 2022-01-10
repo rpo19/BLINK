c27e7653bedbb14227ac04a6bffeccbeea21143b
Riccardo Pozzi <pozzir00@gmail.com>
2022-01-10 19:28:01 +0100

    Add tint to spacy

--- microservices/spacyner/__main__.py
+++ microservices/spacyner/__main__.py
@@ -7,6 +7,10 @@ import spacy
 import sys
 import preprocess
 import itertools
+import json
+import requests
+# from multiprocessing import Pool
+from .entity import EntityMention
 
 class Item(BaseModel):
     text: Union[List[str], str]
@@ -70,6 +74,32 @@ async def encode_mention(item: Item):
 
             samples.append(sample)
 
+        # TODO make it async inside spacy
+        res_tint = nlp_tint(sentence)
+        for ent in res_tint:
+            if ent.type_ == 'DATE':
+                # only dates
+                start_pos_original, end_pos_original = preprocess.mapSpan((ent.begin, ent.end), _mapping_back)
+                sample = {
+                    'label': 'unknown',
+                    'label_id': -1,
+                    'context_left': sentence[:ent.begin],
+                    'context_right': sentence[ent.end:],
+                    'mention': ent.text,
+                    'start_pos': ent.begin,
+                    'end_pos': ent.end,
+                    'start_pos_original': start_pos_original,
+                    'end_pos_original': end_pos_original,
+                    'sent_idx': i,
+                    'ner_type': ent.type_,
+                    'normalized_date': ent.attrs['normalized_date']
+                }
+
+                samples.append(sample)
+            else:
+                # TODO only dates for now
+                continue
+
     return {
         'ents': samples,
         'sentences': sentences,
@@ -77,6 +107,39 @@ async def encode_mention(item: Item):
             'mapping': mapping
         }
     }
+
+def nlp_tint(text):
+    global args
+
+    # TODO async
+    # tint_async = pool.apply_async(tint, (x, args.tint))
+    # res_tint = tint_async.get()
+
+    ents, res = tint(text, args.tint)
+
+    if res.ok:
+        ents = EntityMention.group_from_tint(ents, '', False, doc=text)
+    else:
+        # tint error # TODO
+        return []
+
+    return ents
+
+def tint(text, format_='json', baseurl='http://127.0.0.1:8012/tint'):
+    if len(text) > 0:
+        payload = {
+            'text': text,
+            'format': format_
+        }
+        res = requests.post(baseurl, data=payload)
+        if res.ok:
+            # success
+            return json.loads(res.text), res
+        else:
+            return None, res
+    else:
+        print('WARNING: Tint Server Wrapper got asked to call tint with empty text.', file=sys.stderr)
+        return None, None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -89,6 +152,12 @@ if __name__ == '__main__':
     parser.add_argument(
         "--model", type=str, default="en_core_web_sm", help="spacy model to load",
     )
+    parser.add_argument(
+        "--tint", type=str, default="http://127.0.0.1:8012/tint", help="tint URL",
+    )
+
+    # pool to run tint in parallel # TODO
+    #pool = Pool(1)
 
     args = parser.parse_args()
 
