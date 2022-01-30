fb23c769903498ec7d9d1ac586743ffe4e8dbbc5
rpo <pozzir00@gmail.com>
2022-01-30 11:48:35 +0100

    Add wikidata qid to db

--- microservices/postgres/init.sql
+++ microservices/postgres/init.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS entities (
     id INT NOT NULL,
     indexer INT NOT NULL,
     wikipedia_id INT NOT NULL,
+    wikidata_qid INT,
     title varchar(100) NOT NULL,
     descr TEXT,
     type_ varchar(20),
@@ -10,3 +11,4 @@ CREATE TABLE IF NOT EXISTS entities (
 );
 
 CREATE INDEX IF NOT EXISTS entities_wikipedia_id ON public.entities USING btree (wikipedia_id);
+CREATE INDEX IF NOT EXISTS entities_wikidata_qid ON public.entities USING btree (wikidata_qid);
